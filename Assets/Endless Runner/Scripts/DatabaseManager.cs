84094f2463af61589465924b0ef8748d65002131
Alexis Stahnke <alexis.stahnke@protonmail.com>
2019-05-26 12:24:18 +0200

    Added level 2 prefabs
    Added level 2 prefabs and building level 2 stage.

--- Assets/Endless Runner/Scripts/DatabaseManager.cs
+++ Assets/Endless Runner/Scripts/DatabaseManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DatabaseManager : MonoBehaviour
 {
@@ -12,6 +13,18 @@ public class DatabaseManager : MonoBehaviour
     public string ConnectionDatabaseName;
     public string ConnectionUsername;
     public string ConnectionPassword;
+    [Space(10)]
+    [Header("Insert Information")]
+    public string insertTableName;
+    public string InsertID;
+    public string InsertValue;
+    public string InsertName;
+    [Space(10)]
+    [Header("Select Information")]
+    public string selectTableName;
+    public string selectColumnName;
+    [Space(10)]
+    public Text uiText;
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +45,54 @@ public class DatabaseManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InsertScore()
     {
-        
+        string connetionString = null;
+        connetionString = "server=" + ConnectionServerName + ";database=" + ConnectionDatabaseName + ";uid=" + ConnectionUsername + ";pwd=" + ConnectionPassword;
+        MySqlConnection conn = new MySqlConnection(connetionString);
+        try
+        {
+            Debug.Log("Starting Connection");
+            conn.Open();
+            Debug.Log("Inserting Into Database");
+            string sql = "INSERT INTO " + insertTableName + " VALUES(" + InsertID + ", " + InsertValue + ", " + InsertName + ")";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            Debug.Log("Score added");
+            conn.Close();
+            ShowScore();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Debug not working");
+        }
+    }
+
+    private void ShowScore()
+    {
+        string connetionString = null;
+        connetionString = "server=" + ConnectionServerName + ";database=" + ConnectionDatabaseName + ";uid=" + ConnectionUsername + ";pwd=" + ConnectionPassword;
+        MySqlConnection conn = new MySqlConnection(connetionString);
+        try
+        {
+            Debug.Log("Starting Connection");
+            conn.Open();
+            Debug.Log("Selecting Into Database");
+            string sql = "SELECT " + selectColumnName + " FROM " + selectTableName;
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Debug.Log(rdr[0]);
+                uiText.text = "Highscore: " + rdr[0].ToString();
+            }
+
+            rdr.Close();
+            conn.Close();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Score could not be gotten");
+        }
     }
 }
